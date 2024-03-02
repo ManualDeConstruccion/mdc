0c95220d1c752ff21339d7b5a017d87218108129
paul <pauleatonc@gmail.com>
2024-03-02 02:49:23 -0300

    WIP: Adding order to models section, subsection and titles

--- applications/posts/admin.py
+++ applications/posts/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Article, Section, Subsection, Title
 
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'orden', 'app') 
+    ordering = ('orden',)
+
 admin.site.register(Article)
-admin.site.register(Section)
+admin.site.register(Section, SectionAdmin)
 admin.site.register(Subsection)
 admin.site.register(Title)
+
--- applications/sitecomponents/models.py
+++ applications/sitecomponents/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.apps import apps
 
 class App(models.Model):
     name = models.CharField(max_length=50)
     icon_name = models.CharField(max_length=50)
+
+    @property
+    def ordered_sections(self):
+        Section = apps.get_model('posts', 'Section')
+        return Section.objects.filter(app=self).order_by('orden')
 
     def __str__(self):
         return self.name
