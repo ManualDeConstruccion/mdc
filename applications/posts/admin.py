e71575267a3fc6fb887b42ff0812186d6911d8c9
paul <pauleatonc@gmail.com>
2024-03-03 00:34:32 -0300

    WIP: Setting articles structure

--- applications/posts/admin.py
+++ applications/posts/admin.py
@@ -1,12 +1,42 @@
 from django.contrib import admin
-from .models import Article, Section, Subsection, Title
+from .models import Article, Section, Subsection, Element, Image, File, Video
 
 class SectionAdmin(admin.ModelAdmin):
     list_display = ('name', 'orden', 'app') 
     ordering = ('orden',)
 
-admin.site.register(Article)
+class SubsectionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'orden', 'section') 
+    ordering = ('orden',)
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1  # Número de campos extra para subir imágenes
+    fields = ['image']  # Especifica solo los campos que quieres mostrar
+
+class FileInline(admin.TabularInline):
+    model = File
+    extra = 1
+    fields = ['file', 'name']  # Especifica solo los campos que quieres mostrar
+
+class VideoInline(admin.TabularInline):
+    model = Video
+    extra = 1
+    fields = ['url']  # Especifica solo los campos que quieres mostrar    
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'orden', 'subsection') 
+    ordering = ('orden',)
+    inlines = [ImageInline, FileInline, VideoInline]
+
+class ElementAdmin(admin.ModelAdmin):
+    inlines = [ImageInline, FileInline, VideoInline]
+    list_display = ('name', 'article') 
+
+# Registrar cada modelo una vez
 admin.site.register(Section, SectionAdmin)
-admin.site.register(Subsection)
-admin.site.register(Title)
+admin.site.register(Subsection, SubsectionAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Element, ElementAdmin)
+
 
--- applications/posts/models.py
+++ applications/posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from applications.sitecomponents.models import App
 
+
 class Section(models.Model):
     name = models.CharField(max_length=140, unique=True, null=False, blank=False)
     app = models.ForeignKey(App, on_delete=models.CASCADE, related_name='sections', null=True)
@@ -17,22 +18,61 @@ class Subsection(models.Model):
     def __str__(self):
         return self.name
 
-class Title(models.Model):
+class Article(models.Model):
     name = models.CharField(max_length=200, unique=True, null=False, blank=False)
-    subsection = models.ForeignKey(Subsection, on_delete=models.CASCADE, related_name='titles')
+    subsection = models.ForeignKey(Subsection, on_delete=models.CASCADE, related_name='articles', null=True)
     orden = models.IntegerField(default=0, null=False, blank=False)
+    description = models.TextField(max_length=3000, null=True, blank=False)
+    reference = models.TextField(max_length=200, blank=True, null=True) # Opcional
+
+    def delete(self, *args, **kwargs):
+        # Eliminar todas las imágenes relacionadas
+        for image in self.images.all():
+            image.delete()
+        # Eliminar todos los archivos relacionados
+        for file in self.files.all():
+            file.delete()
+        # Eliminar todos los videos relacionados
+        for video in self.videos.all():
+            video.delete()
+        super(Article, self).delete(*args, **kwargs)
 
     def __str__(self):
         return self.name
     
-class Article(models.Model):
+class Element(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False)
     description = models.TextField(max_length=3000, null=False, blank=False)
-    reference = models.URLField(max_length=200, blank=True, null=True) # Opcional
-    images = models.ImageField(upload_to='articles_images/', blank=True, null=True) # Opcional
-    video = models.FileField(upload_to='articles_videos/', blank=True, null=True) # Opcional
-    files = models.FileField(upload_to='articles_files/', blank=True, null=True) # Opcional
-    title = models.ForeignKey(Title, on_delete=models.CASCADE, related_name='articles')
+    reference = models.TextField(max_length=200, blank=True, null=True) # Opcional
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='elements', null=True)
+
+    def delete(self, *args, **kwargs):
+        # Eliminar todas las imágenes relacionadas
+        for image in self.images.all():
+            image.delete()
+        # Eliminar todos los archivos relacionados
+        for file in self.files.all():
+            file.delete()
+        # Eliminar todos los videos relacionados
+        for video in self.videos.all():
+            video.delete()
+        super(Element, self).delete(*args, **kwargs)
 
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    article = models.ForeignKey('Article', related_name='article_images', on_delete=models.CASCADE, null=True, blank=True)
+    element = models.ForeignKey('Element', related_name='element_images', on_delete=models.CASCADE, null=True, blank=True)
+
+class File(models.Model):
+    name = models.CharField(null=True, blank=False, max_length=200, verbose_name='Nombre (obligatorio)', unique=True)
+    file = models.FileField(upload_to='files/')
+    article = models.ForeignKey('Article', related_name='article_files', on_delete=models.CASCADE, null=True, blank=True)
+    element = models.ForeignKey('Element', related_name='element_files', on_delete=models.CASCADE, null=True, blank=True)
+
+class Video(models.Model):
+    url = models.URLField()
+    article = models.ForeignKey('Article', related_name='article_videos', on_delete=models.CASCADE, null=True, blank=True)
+    element = models.ForeignKey('Element', related_name='element_videos', on_delete=models.CASCADE, null=True, blank=True)
