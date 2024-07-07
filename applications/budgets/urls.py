d4ded52084a4b590f1864c9fe0f0434cb240cbeb
JaimeHernandezE <jhearquitecto@gmail.com>
2024-07-07 18:48:18 -0400

    FIX: Templates fuera de las aplicaciones

--- applications/budgets/urls.py
+++ applications/budgets/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import BudgetProjectCreateView, BudgetProjectDetailView
 
+app_name = 'budget'
+
 urlpatterns = [
     path('create/', BudgetProjectCreateView.as_view(), name='create_budget'),
     path('detail/<int:pk>/', BudgetProjectDetailView.as_view(), name='view_budget'),
