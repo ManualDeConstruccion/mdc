6971e589b57bcb58f29c6fdda6be4adb470b425c
paul <pauleatonc@gmail.com>
2024-07-18 00:29:15 -0400

    ADD: Buckets for statics and media

--- buildguide/asgi.py
+++ buildguide/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buildguide.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buildguide.settings.local')
 
 application = get_asgi_application()
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 # Ejecuta collectstatic para reunir archivos estáticos
 python manage.py collectstatic --noinput
 
+# Verifica la existencia de la carpeta y lista su contenido
+if [ -d "/app/staticfiles" ]; then
+    echo "La carpeta staticfiles existe y contiene los siguientes archivos:"
+    ls -l /app/staticfiles
+else
+    echo "La carpeta staticfiles no se creó."
+fi
+
 # Ejecuta el comando original pasado como argumentos a este script
 exec "$@"
